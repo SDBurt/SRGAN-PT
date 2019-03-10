f67fd83129f7387ec870004dc03d24434dbe58fd
SDBurt <seandburt@gmail.com>
2019-03-10 13:12:41 -0700

    add discriminator network

--- models.py
+++ models.py
@@ -34,9 +34,9 @@ class Flatten(nn.Module):
         return input.view(input.size(0), -1)
 
 
-class ResNet(nn.Module):
+class GeneratorNetwork(nn.Module):
     def __init__(self, input, num_classes):
-        super(ResNet, self).__init__()
+        super(GeneratorNetwork, self).__init__()
         self.conv1 = nn.Conv2d(input, 16, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(16)
         self.relu = nn.ReLU(inplace=True)
@@ -78,3 +78,77 @@ class ResNet(nn.Module):
         x = self.fc2(x)
 
         return x
+
+class DiscriminatorNetwork(nn.Module):
+    def __init__(self, input, num_classes):
+        super(DiscriminatorNetwork, self).__init__()
+
+        self.conv1 = nn.Conv2d(input, 64, kernel_size=3, stride=1)
+        self.lrelu = nn.LeakyReLU()
+
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=2)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, stride=2)
+        self.bn4 = nn.BatchNorm2d(128)
+
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, stride=1)
+        self.bn5 = nn.BatchNorm2d(256)
+        self.conv6 = nn.Conv2d(256, 256, kernel_size=3, stride=2)
+        self.bn6 = nn.BatchNorm2d(256)
+
+        self.conv7 = nn.Conv2d(256, 512, kernel_size=3, stride=1)
+        self.bn7 = nn.BatchNorm2d(512)
+        self.conv8 = nn.Conv2d(512, 512, kernel_size=3, stride=2)
+        self.bn8 = nn.BatchNorm2d(512)
+
+        self.flatten = Flatten()
+
+        # pytorch does not have a good flatten into dense
+        # method so this is a TODO
+        self.fc1 = nn.Linear(3872, 1024)
+
+        self.fc2 = nn.Linear(1024, 1)
+        self.sigm = nn.Sigmoid()
+
+    def forward(self, x):
+        # k3 n64 s1
+        x = self.conv1(x)
+        x = self.lrelu(x)
+        # k3 n64 s2
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.lrelu(x)
+        # k3 n128 s1
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.lrelu(x)
+        # k3 n128 s2
+        x = self.conv4(x)
+        x = self.bn4(x)
+        x = self.lrelu(x)
+        # k3 n256 s1
+        x = self.conv5(x)
+        x = self.bn5(x)
+        x = self.lrelu(x)
+        # k3 n256 s2
+        x = self.conv6(x)
+        x = self.bn6(x)
+        x = self.lrelu(x)
+        # k3 n512 s1
+        x = self.conv7(x)
+        x = self.bn7(x)
+        x = self.lrelu(x)
+        # k3 n512 s2
+        x = self.conv8(x)
+        x = self.bn8(x)
+        x = self.lrelu(x)
+        # flatten, dense(1024), lrelu, dense(1) sigmoid
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.lrelu(x)
+        x = self.fc2(x)
+        
+        return self.sigm(x)
