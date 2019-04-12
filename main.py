2b5906bd66f4db872f1b77a523c4bd605b8860b1
sburt <sburt@uvic.ca>
2019-04-12 15:42:49 -0700

    update requirements.txt

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import torch as tr
 import torchvision as tv
 import numpy as np
-import cv2
-import random
-import os
-import h5py
+import cv2, random, os, h5py
 from tqdm import trange
 from pathlib import Path
 from tensorboardX import SummaryWriter
@@ -13,7 +10,7 @@ from models import Generator, Discriminator
 from preprocessing import package_data
 from torchvision.models.vgg import vgg19
 
-device = tr.device("cuda" if tr.cuda.is_available() else "cpu")
+device = tr.device('cuda' if tr.cuda.is_available() else 'cpu')
 cfg = get_config()
 
 
@@ -24,6 +21,8 @@ class SRGAN(object):
         self.generator = Generator(cfg).to(device)
         self.discriminator = Discriminator(cfg).to(device)
 
+        self.vgg = vgg19(pretrained=True)
+
         self.preprocessing()
 
     def preprocessing(self):
@@ -33,10 +32,10 @@ class SRGAN(object):
 
         # Load data
         cwd = os.getcwd()
-        f = h5py.File(cwd + cfg.data_dir + "/data.h5", "r")
-        lr = f["lr"][:]
-        hr = f["hr"][:]
-        ds = f["ds"][:]
+        f = h5py.File(cwd + cfg.data_dir + '/data.h5', 'r')
+        lr = f['lr'][:]
+        hr = f['hr'][:]
+        ds = f['ds'][:]
         f.close()
 
         self.data_tr = list(zip(hr, ds))
@@ -53,23 +52,11 @@ class SRGAN(object):
 
     def train(self):
 
-        self.generator().train()
-        self.discriminator().train()
-
-        mse_loss = tr.nn.MSELoss()
-        vgg = Vgg16(pretrained=True)
-
         for epoch in trange(cfg.epochs):
-
             batch = self.get_batch()
-            content_loss = 0
-            adversarial_loss = 0
-
             for hr, ds in batch:
                 # HWC -> NCHW, make type torch.cuda.float32
-                ds = (
-                    tr.tensor(ds[None], dtype=tr.float32).permute(0, 3, 1, 2).to(device)
-                )
+                ds = tr.tensor(ds[None], dtype=tr.float32).permute(0, 3, 1, 2).to(device)
                 sr = self.generator(ds)
 
                 # Discriminate between the real and generated fake image
@@ -77,22 +64,20 @@ class SRGAN(object):
                 real_img = self.discriminator(hr)
 
                 # Perceptual Loss (VGG loss), which is content loss + 10e-3 * adversarial
-                f_real = vgg(hr)  # VGG features for real image
-                f_fake = vgg(sr)  # VGG features for fake image
+                f_real = self.vgg(hr)  # VGG features for real image
+                f_fake = self.vgg(sr)  # VGG features for fake image
 
                 # content loss euclidean distance between features
-                content_loss += mse_loss(f_real.relu2_2, f_fake.relu2_2)
-                adversarial_loss += -np.log(f_fake)
+                content_loss = tr.nn.MSELoss(f_real.relu2_2, f_fake.relu2_2)
+                adversarial_loss = -np.log(f_fake)
 
-            # Loss
-            loss = content_loss + (10e-3 * adversarial_loss)
-            loss.backwards()
+                loss = content_loss + (10e-3 * adversarial_loss)
+                loss.backwards()
 
 
 def main():
     srgan = SRGAN(cfg)
     srgan.train()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
